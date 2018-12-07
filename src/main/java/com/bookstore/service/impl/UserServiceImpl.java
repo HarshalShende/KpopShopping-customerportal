b006c04fab85e212a4d51c6c7b52c2b938c3d287
tiansss <tianshi.st1020@gmail.com>
2018-12-07 12:53:38 -0500

    CreateUser and email related functions

--- src/main/java/com/bookstore/service/impl/UserServiceImpl.java
+++ src/main/java/com/bookstore/service/impl/UserServiceImpl.java
@@ -1,15 +1,26 @@
 package com.bookstore.service.impl;
 
+import java.util.Set;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.PasswordResetToken;
+import com.bookstore.domain.security.UserRole;
 import com.bookstore.repository.PasswordResetTokenRepository;
+import com.bookstore.repository.RoleRepository;
+import com.bookstore.repository.UserRepository;
 import com.bookstore.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private RoleRepository roleRepository;
 	
 	@Autowired
 	private PasswordResetTokenRepository passwordResetTokenRepository;
@@ -23,5 +34,32 @@ public class UserServiceImpl implements UserService{
 	public void createPasswordResetTokenForUser(final User user, final String token) {
 		final PasswordResetToken myToken = new PasswordResetToken(token, user);
 		passwordResetTokenRepository.save(myToken);
+	}
+	
+	@Override
+	public User findByUsername(String username) {
+		return userRepository.findByUsername(username);
+	}
+	
+	@Override
+	public User findByEmail(String email) {
+		return userRepository.findByEmail(email);
+	}
+	
+	@Override
+	public User createUser(User user, Set<UserRole> userRoles) throws Exception{
+		User localUser = userRepository.findByUsername(user.getUsername());
+		
+		if (localUser != null){ 
+			throw new Exception("user already exists. Nothing will be done");
+		}
+		else {
+			for (UserRole ur: userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			user.getUserRoles().addAll(userRoles);
+			localUser = userRepository.save(user);
+		}
+		return localUser;
 	}
 }
